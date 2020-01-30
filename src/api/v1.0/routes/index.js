4b6e07d6d9915833f100a5368d20d9fa139dc8bc
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2020-01-30 08:39:48 +0100

    set up admin blog page

--- src/api/v1.0/routes/index.js
+++ src/api/v1.0/routes/index.js
@@ -11,10 +11,10 @@ module.exports = (app) => {
   app.post('/api/login', authController.login);
   app.post('/api/register', authController.register);
   app.get('/api/users', authController.userList);
-  app.post('/api/draft', draftController.createDraft);
-  app.get('/api/draft/:id', draftController.getDraft);
-  app.put('/api/draft/:id', draftController.updateDraft);
-  app.get('/api/draft', draftController.getAllDrafts);
+  // app.post('/api/draft', draftController.createDraft);
+  // app.get('/api/draft/:id', draftController.getDraft);
+  // app.put('/api/draft/:id', draftController.updateDraft);
+  // app.get('/api/draft', draftController.getAllDrafts);
   app.get('/api/checkToken', authMiddleware.checkAuth, (req, res) => {
     res.sendStatus(200);
   });
