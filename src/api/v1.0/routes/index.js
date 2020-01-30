1948cbff71da3ae4938d169e6f9bd50464acfb92
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2020-01-30 02:48:33 +0100

    set up admin blog page

--- src/api/v1.0/routes/index.js
+++ src/api/v1.0/routes/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable linebreak-style */
 const authController = require('../controllers').users;
+const draftController = require('../controllers/blogdraft');
 const authMiddleware = require('../middlewares/auth');
 
 module.exports = (app) => {
@@ -10,6 +11,10 @@ module.exports = (app) => {
   app.post('/api/login', authController.login);
   app.post('/api/register', authController.register);
   app.get('/api/users', authController.userList);
+  app.post('/api/draft', draftController.createDraft);
+  app.get('/api/draft/:id', draftController.getDraft);
+  app.put('/api/draft/:id', draftController.updateDraft);
+  app.get('/api/draft', draftController.getAllDrafts);
   app.get('/api/checkToken', authMiddleware.checkAuth, (req, res) => {
     res.sendStatus(200);
   });
