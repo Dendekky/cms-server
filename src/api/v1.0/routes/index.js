31d442fe26fa5d63e3a89b10da82d97b46816733
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2020-02-01 10:56:27 +0100

    feat: create route for published post

--- src/api/v1.0/routes/index.js
+++ src/api/v1.0/routes/index.js
@@ -12,12 +12,17 @@ module.exports = (app) => {
   app.post('/api/login', authController.login);
   app.post('/api/register', authController.register);
   app.get('/api/users', authController.userList);
+  // Drafts routes
   app.post('/api/draft', draftController.createDraft);
   app.get('/api/draft/:id', draftController.getDraft);
   app.put('/api/draft/:id', draftController.updateDraft);
   app.get('/api/draft', draftController.getAllDrafts);
-
+// Posts routes
   app.get('/api/post', getAllPosts);
+  api.post('/api/post', createPost);
+  api.get('/api/post/:id', getPost);
+  api.put('/api/post/:id', updatePost);
+
   app.get('/api/checkToken', authMiddleware.checkAuth, (req, res) => {
     res.sendStatus(200);
   });
