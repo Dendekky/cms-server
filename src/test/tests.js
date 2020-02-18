7d4eafa31945e3c84d05f0a184bff3742701b305
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2020-02-18 16:06:52 +0100

    feat: add more unit tests

--- src/test/tests.js
+++ src/test/tests.js
@@ -7,16 +7,18 @@ var expect = chai.expect;
 chai.use(assertArrays);
 chai.use(chaiHttp);
 import BlogDraft from '../api/v1.0/models/blogdraft';
+import BlogPost from '../api/v1.0/models/blogpost';
 
-
-
-const draft = new BlogDraft({
+const blog = {
   title: "Hello World",
   category: "Test",
   body: "Testing This Guy",
   metadata: "hello who is the man in the building",
   postImage: "well.jpg",
-});
+}
+const draft = new BlogDraft(blog);
+
+const post = new BlogPost(blog);
 
 describe('Mm server', async () => {
 
@@ -32,83 +34,141 @@ describe('Mm server', async () => {
       });
   });
 
-  // await describe('Post draft', () => {
-  //   it('it should save a draft', (done) => {
-  //     draft.save((err, draft) => {
-  //     chai.request(server)
-  //       .post('/api/draft/')
-  //       .end((err, res) => {
-  //         res.should.have.status(201);
-  //         expect(res.body).to.deep.equal( { status: 201, success: 'saved to draft' } );
-  //         done();
-  //       });
-  //     });
-  //   });
-  // });
+  describe('Test for Drafts', () => {
+    describe('Post draft', () => {
+      it('it should save a draft', (done) => {
+        chai.request(server)
+          .post('/api/draft/')
+          .send(draft)
+          .end((err, res) => {
+            res.should.have.status(201);
+            expect(res.body).to.deep.equal( { status: 201, success: 'saved to draft' } );
+            done();
+          });
+      });
+    });
 
-  await describe('Update draft', () => {
-    it('it should update a draft', (done) => {
-      draft.save((err, draft) => {
-      chai.request(server)
-        .put('/api/draft/' + draft.id)
-        .send({ title: "The Chronicles of Narnia",
-                    category: "Test",
-                    body: "Testing This Guy",
-                    metadata: "hello who is the man in the building" 
-                  })
-        .end((err, res) => {
-          res.should.have.status(201);
-          expect(res.body).to.deep.equal( { message: 'update successful' } );
-          done();
+    describe('Update draft', () => {
+      it('it should update a draft', (done) => {
+        draft.save((err, draft) => {
+        chai.request(server)
+          .put('/api/draft/' + draft.id)
+          .send({ title: "The Chronicles of Narnia",
+                      category: "Test",
+                      body: "Testing This Guy",
+                      metadata: "hello who is the man in the building" 
+                    })
+          .end((err, res) => {
+            res.should.have.status(201);
+            expect(res.body).to.deep.equal( { message: 'update successful' } );
+            done();
+          });
         });
+      });
+    });
+
+    describe('Get draft', () => {
+      it('it should get a draft', (done) => {
+        chai.request(server)
+          .get('/api/draft/' + draft.id)
+          .end((err, res) => {
+            res.should.have.status(200);
+            done();
+          });
+      });
+    });
+    
+    describe('Delete draft', () => {
+      it('it should delete a draft', (done) => {
+        chai.request(server)
+          .delete('/api/draft/' + draft.id)
+          .end((err, res) => {
+            res.should.have.status(200);
+            expect(res.body).to.deep.equal( { message: 'post deleted' } );
+            done();
+          });
+      });
+    });
+    
+    describe('All Drafts', () => {
+      it('it should get all drafts', (done) => {
+            chai.request(server)
+            .get('/api/draft')
+            .end((err, res ) => {
+                  res.should.have.status(200);
+                  expect(res.body.drafts).to.be.array();
+              done();
+            });
       });
     });
   });
 
-  await describe('Get draft', () => {
-    it('it should get a draft', (done) => {
-      chai.request(server)
-        .get('/api/draft/' + draft.id)
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
-  });
-  
-  await describe('Delete draft', () => {
-    it('it should delete a draft', (done) => {
-      chai.request(server)
-        .delete('/api/draft/' + draft.id)
-        .end((err, res) => {
-          res.should.have.status(200);
-          expect(res.body).to.deep.equal( { message: 'post deleted' } );
-          done();
-        });
-    });
-  });
-  
-  describe('All Drafts', () => {
-    it('it should get all drafts', (done) => {
-          chai.request(server)
-          .get('/api/draft')
-          .end((err, res ) => {
-                res.should.have.status(200);
-                expect(res.body.drafts).to.be.array();
+  describe('Test for BlogPosts', () => {
+    describe('save post', () => {
+      it('it should save a post', (done) => {
+        chai.request(server)
+          .post('/api/post/')
+          .send(post)
+          .end((err, res) => {
+            res.should.have.status(201);
+            expect(res.body).to.deep.equal( { status: 201, success: 'saved to post' } );
             done();
           });
+      });
     });
-  });
 
-  describe('All Posts', () => {
-    it('it should get all posts', (done) => {
-          chai.request(server)
-          .get('/api/post')
-          .end((err, res ) => {
-                res.should.have.status(200);
-                expect(res.body.posts).to.be.array();
+    describe('Update post', () => {
+      it('it should update a post', (done) => {
+        post.save((err, post) => {
+        chai.request(server)
+          .put('/api/post/' + post.id)
+          .send({ title: "The Chronicles of Narnia",
+                      category: "Test",
+                      body: "Testing This Guy",
+                      metadata: "hello who is the man in the building" 
+                    })
+          .end((err, res) => {
+            res.should.have.status(201);
+            expect(res.body).to.deep.equal( { message: 'update successful' } );
             done();
           });
+        });
+      });
+    });
+
+    describe('Get post', () => {
+      it('it should get a draft', (done) => {
+        chai.request(server)
+          .get('/api/post/' + post.id)
+          .end((err, res) => {
+            res.should.have.status(200);
+            done();
+          });
+      });
+    });
+    
+    describe('Delete post', () => {
+      it('it should delete a post', (done) => {
+        chai.request(server)
+          .delete('/api/post/' + post.id)
+          .end((err, res) => {
+            res.should.have.status(200);
+            expect(res.body).to.deep.equal( { message: 'post deleted' } );
+            done();
+          });
+      });
+    });
+
+    describe('All Posts', () => {
+      it('it should get all posts', (done) => {
+            chai.request(server)
+            .get('/api/post')
+            .end((err, res ) => {
+                  res.should.have.status(200);
+                  expect(res.body.posts).to.be.array();
+              done();
+            });
+      });
     });
   });
 
