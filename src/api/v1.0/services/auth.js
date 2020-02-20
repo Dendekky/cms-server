fcf9df9bce61430bc3fd5e32e060532db8e3a552
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2020-02-20 12:00:25 +0100

    feat: protect admin routes

--- src/api/v1.0/services/auth.js
+++ src/api/v1.0/services/auth.js
@@ -1,23 +1,19 @@
 /* eslint-disable linebreak-style */
 import Users from '../models/user';
 
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
-exports.authenticate = params => Users.findOne({
-  email: params.email,
-})
-  .then((user) => {
-    if (!user) { throw new Error('Authentication failed. User not found.'); }
-    if (!bcrypt.compareSync(params.password || '', user.password)) { throw new Error('Authentication failed. Wrong password.'); }
+exports.authenticate = params => {
     const payload = {
-      email: user.email,
-      id: user.id,
+      username: params.username,
+      id: params.password,
       time: new Date(),
     };
     const token = jwt.sign(payload, config.development.jwtSecret || config.production.jwtSecret, {
       expiresIn: config.development.tokenExpireTime || config.production.tokenExpireTime,
     });
     return token;
-  });
+  // });
+}
